b08d906f890e0791e4eb5ee6de2ea629b92abf37
SilmarwenEmi <laurent.emilie8@gmail.com>
2020-03-04 14:29:53 +0100

    auto subject with space

--- main.py
+++ main.py
@@ -11,17 +11,45 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy.graphics import Rectangle, Color, RoundedRectangle
 
+#from kivy.properties import StringProperty
+
 
 from kivy.core.window import Window
 Window.clearcolor = (.94, .94, .94, 1)
 
+class Subject(Widget):
+    pass
 
-class MyGrid(Widget):
+class Topics(Widget):
+    pass
+
+class Header(Widget):
+    pass
+
+class MainBox(BoxLayout):
     pass
 
 class MyApp(App): # <- Main Class
     def build(self):
-        return MyGrid()
+                
+        mainBox = MainBox()
+
+        header = Header()
+
+        mainBox.ids.top_box.add_widget(header)
+
+        topics = Topics()
+
+        header.ids.content_box.add_widget(topics)
+
+        listSubject = []
+        for cpt in range (6):
+            listSubject.append(Subject())
+
+        for subject in listSubject:
+            topics.ids.contents.add_widget(subject)
+
+        return mainBox
 
 
 if __name__ == "__main__":
