d39db8b67ababe22e94e13c52bb1e1e314353266
SilmarwenEmi <laurent.emilie8@gmail.com>
2020-03-06 10:20:13 +0100

    subject box done without line

--- main.py
+++ main.py
@@ -12,11 +12,24 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy.graphics import Rectangle, Color, RoundedRectangle
 
+from kivy.uix.screenmanager import Screen, ScreenManager
+
 from kivy.properties import StringProperty, ListProperty
+from kivy.lang import Builder
 
 
 from kivy.core.window import Window
 Window.clearcolor = (.94, .94, .94, 1)
+
+"""class WindowManager(ScreenManager):
+    pass
+
+class MainScreen(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass"""
+
 
 
 class Subject(Widget):
@@ -35,7 +48,9 @@ class MyApp(App): # <- Main Class
     subjectsTitles = ListProperty()
     def build(self):
 
-        abcd = StringProperty('test')
+        subjectName = StringProperty('test')
+        subjectCamNb = StringProperty('test1')
+        subjectGrafNb = StringProperty('test2')
 
         mainBox = MainBox()
 
@@ -45,10 +60,18 @@ class MyApp(App): # <- Main Class
         topics = Topics()
         header.ids.content_box.add_widget(topics)
 
-        subjectsTitles = ["Emi", "Laurent", "Hawa", "Kirby", "Jack", "Nala"]
+        subjectsTitles = [["Les 50 ans de la faculté d'informatique", "10", "2"], 
+                          ["La réforme du cursus en informatique","1", "3"], 
+                          ["Master en sciences informatiques à Charleroi","1", "3"], 
+                          ["Kirby","1", "3"], 
+                          ["Jack","1", "3"], 
+                          ["Nala","1", "3"]
+                        ]
         listSubject = []
         for i in subjectsTitles:
-            self.abcd = i
+            self.subjectName = i[0]
+            self.subjectCamNb = i[1]
+            self.subjectGrafNb = i[2]
             listSubject.append(Subject())
 
         for subject in listSubject:
